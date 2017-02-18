538b1893e1b8648469c1d44bed0157787773065b
Empty Keys <github@emptykeys.com>
2017-02-18 17:30:54 +0100

    Custom Mod Data Xenko project

---
+++ Custom_Mod_Data/Custom_Mod_Data/Custom_Mod_Data.Windows/Custom_Mod_DataApp.cs
@@ -0,0 +1,15 @@
+using SiliconStudio.Xenko.Engine;
+
+namespace Custom_Mod_Data
+{
+    class Custom_Mod_DataApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
